d1853982807b8a1f9e4e2968a93787c261a70236
Admin <admin@Mac-Admin.local>
2015-04-02 17:38:26 +0300

    Initial Commit

---
+++ CallingApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CallingApp
+//
+//  Created by Admin on 02.04.15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CallingApp/SecondViewController.h
@@ -0,0 +1,15 @@
+//
+//  SecondViewController.h
+//  CallingApp
+//
+//  Created by Admin on 02.04.15.
+//  Copyright (c) 2015 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+
+@end
+
